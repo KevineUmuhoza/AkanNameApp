c86e417073e17c4b926b1edb8df0335c0f0b9849
KevineUmuhoza <ckevny@gmail.com>
2020-12-22 03:56:48 +0200

    declared arrays of akan names

--- js/script.js
+++ js/script.js
@@ -18,3 +18,7 @@ DD = birthDay
 dayOfWeek = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7;
 dayOfWeek = parseInt(dayOfWeek);
 
+var akanNmame;
+maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
